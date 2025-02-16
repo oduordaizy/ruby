907d382e1255172c6a61d02adc4d46e905e55a0a
achdaisy <oduordaizy@gmail.com>
2025-02-16 20:54:15 +0300

    Handling errors and exceptions

---
+++ exceptions.rb
@@ -0,0 +1,11 @@
+#To create a try catch statements you"
+begin
+  puts "begin"
+  puts 3/1
+rescue
+  puts "Error catched"
+else
+  puts "Looks good"
+ensure
+  puts "Always runs irrespective of above"
+end
